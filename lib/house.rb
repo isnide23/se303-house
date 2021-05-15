f2d48e9309d946cdcbd8c417ca09d5f50019cfea
Ian Snyder <isnide23@gmail.com>
2021-05-15 08:27:23 -0700

    add start words from constructor and call on each case statement to start the verse

--- lib/house.rb
+++ lib/house.rb
@@ -5,13 +5,9 @@ class House
         @start_words = start_words
     end
 
-    def lyrics(number)
-        "#{@start_words}" 
-    end
-
     def push_lines_to_segments
-        @segments[0] = "in the house that Jack built."
-        @segments[1] = "the malt that lay "
+        @segments[0] = "the house that Jack built."
+        @segments[1] = "the malt that lay in"
         @segments[2] = "the rat that ate "
         @segments[3] = "the cat that killed" 
         @segments[4] = "the dog that worried"
@@ -27,29 +23,29 @@ class House
     def line(number)
         case number
         when 1
-            "This is the house that Jack built.\n"
+            @start_words + " the house that Jack built.\n"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            @start_words + " the malt that lay in the house that Jack built.\n"
         when 3
-            "This is the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
-            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 5
-            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 6
-            "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 7
-            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 8
-            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 9
-            "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 10
-            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 11
-            "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 12
-            "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            @start_words + " the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
             "not a valid line number"
         end
@@ -84,7 +80,4 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
     end
 end
 
-practice = House.new(true)
-practice.push_lines_to_segments
-puts practice.lyrics(3)
 
