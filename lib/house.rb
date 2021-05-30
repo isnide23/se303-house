3bddd159b8b9695611198bc15b36fdd745a9f4dc
Ian Snyder <isnide23@gmail.com>
2021-05-30 16:49:07 -0700

    took out duplicate code smell in RandomHouse

--- lib/house.rb
+++ lib/house.rb
@@ -36,19 +36,9 @@ end
 
 class RandomHouse < House
     def line_segments
-        lines = [
-            'the horse and the hound and the horn that belonged to',
-            'the farmer sowing his corn that kept',
-            'the rooster that crowed in the morn that woke',
-            'the priest all shaven and shorn that married',
-            'the man all tattered and torn that kissed',
-            'the maiden all forlorn that milked',
-            'the cow with the crumpled horn that tossed',
-            'the dog that worried',
-            'the cat that killed',
-            'the rat that ate',
-            'the malt that lay in',
-    ].shuffle.append(@end_words)
+        data = super
+        data.pop
+        data.shuffle.append(@end_words)
     end
 
 end
