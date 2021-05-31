a4387b84da8b8c80a458ae00511298c9899e8154
Ian Snyder <isnide23@gmail.com>
2021-05-30 17:00:42 -0700

    add a subject and verbs array and splice them together

--- lib/house.rb
+++ lib/house.rb
@@ -4,21 +4,56 @@ class House
         @end_words = "the house that Jack built."
     end
 
-    def line_segments
+    def subjects
         [
-        'the horse and the hound and the horn that belonged to',
-        'the farmer sowing his corn that kept',
-        'the rooster that crowed in the morn that woke',
-        'the priest all shaven and shorn that married',
-        'the man all tattered and torn that kissed',
-        'the maiden all forlorn that milked',
-        'the cow with the crumpled horn that tossed',
-        'the dog that worried',
-        'the cat that killed',
-        'the rat that ate',
-        'the malt that lay in',
-        'the house that Jack built.',
+        'the horse and the hound and the horn ',
+        'the farmer sowing his corn ',
+        'the rooster that crowed in the morn ',
+        'the priest all shaven and shorn ',
+        'the man all tattered and torn ',
+        'the maiden all forlorn ',
+        'the cow with the crumpled horn ',
+        'the dog ',
+        'the cat ',
+        'the rat ',
+        'the malt ',
         ]
+    end
+
+    def verbs
+        [
+        'that belonged to',
+        'that kept',
+        'that woke',
+        'that married',
+        'that kissed',
+        'that milked',
+        'that tossed',
+        'that worried',
+        'that killed',
+        'that ate',
+        'that lay in',
+        ]
+    end
+
+
+
+    def line_segments
+        (0..10).map { |i| "#{subjects[i]}#{verbs[i]}"}.append(@end_words)
+        # [
+        # 'the horse and the hound and the horn that belonged to',
+        # 'the farmer sowing his corn that kept',
+        # 'the rooster that crowed in the morn that woke',
+        # 'the priest all shaven and shorn that married',
+        # 'the man all tattered and torn that kissed',
+        # 'the maiden all forlorn that milked',
+        # 'the cow with the crumpled horn that tossed',
+        # 'the dog that worried',
+        # 'the cat that killed',
+        # 'the rat that ate',
+        # 'the malt that lay in',
+        # 'the house that Jack built.',
+        # ]
     end
 
     def phrase(number)
